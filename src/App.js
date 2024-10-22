1153041ba924249570b831199c4b025efcd43e07
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-10-22 17:35:50 +0330

    Weather App React started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const api = {
+  key : "725aa1610a4fee38424409fbbcdaf88d",
+  base:"https://apiopenweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
